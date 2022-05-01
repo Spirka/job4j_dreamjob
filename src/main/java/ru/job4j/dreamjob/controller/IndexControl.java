e9bbb202cc9df4d6c5d217ae39ba96a74abc22a4
Kseniya Dergunova <kdergunova@vocalize.ru>
2022-05-02 01:27:49 +0300

    1. Генератор HTML [#504838]

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -1,7 +1,7 @@
 package ru.job4j.dreamjob.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Class IndexControl
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Kseniya Dergunova
  * @since 02.05.2022
  */
-@RestController
+@Controller
 public class IndexControl {
 
     @GetMapping("/index")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
     }
 }
