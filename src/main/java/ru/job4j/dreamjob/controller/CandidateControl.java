0b509c2aa39c4e3c23f3919c5b75b4fdbdcaabca
Kseniya Dergunova <kdergunova@vocalize.ru>
2022-05-02 14:34:15 +0300

    8. HTML form. Создание кандидата. [#504845]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateControl.java
@@ -3,7 +3,10 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.store.CandidateStore;
+
+import java.time.Instant;
 
 /**
  * Class CandidateControl
@@ -17,8 +20,14 @@ public class CandidateControl {
     private final CandidateStore store = CandidateStore.instOf();
 
     @GetMapping("/candidates")
-    public String posts(Model model) {
+    public String candidate(Model model) {
         model.addAttribute("candidates", store.findAll());
         return "candidates";
+    }
+
+    @GetMapping("/formAddCandidate")
+    public String addCandidate(Model model) {
+        model.addAttribute("candidate", new Candidate(0, "ФИО", "Описание", Instant.now()));
+        return "addCandidate";
     }
 }
